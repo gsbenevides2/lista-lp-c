027b9c210b12615ec391779bed37ff9ae9222c3d
Guilherme da Silva Benevides <45762112+gsbenevides2@users.noreply.github.com>
2022-03-30 16:20:23 -0300

    Aula 1 Exercício 1

---
+++ Aula 1/Exercicio 1/1 obeso.c
@@ -0,0 +1,18 @@
+/* OBESO>C - determina se uma pessoa está obesa */
+
+#include <stdio.h> // entrada e saida padrão
+#include <math.h>  // funções matemáticas
+
+int main(void)
+{
+ float peso, altura, imc;
+ printf("Qual o peso e a altura? ");
+ scanf("%f %f", &peso, &altura);
+ imc = peso / pow (altura, 2);
+ printf("IMC = %.1f\n", imc);
+ if (imc <= 30)
+	printf("Não esta obesa!\n");
+ else
+	printf("Esta obesa!\n");
+ return 0;
+}
