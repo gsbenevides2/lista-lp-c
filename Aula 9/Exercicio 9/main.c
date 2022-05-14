e1aaf54bb43615da4b5fe57b60dc42b0158178f0
Guilherme da Silva Benevides <45762112+gsbenevides2@users.noreply.github.com>
2022-05-13 22:53:53 -0300

    Aula 9 Exercício 9

---
+++ Aula 9/Exercicio 9/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef struct {
+ int origem; int destino;
+} Via;
+
+void exibe(int r[], int n) {
+ for (int i=0; i<n; i++)
+	printf("%d ", r[i]);
+ printf("\b.\n");
+}
+
+int pertence(int x, int r[], int n) {
+ for (int i=0; i<n; i++)
+	if (r[i] == x) return 1;
+ return 0;
+}
+
+void rotas(int x, int y, Via v[], int n){
+ static int r[100], i =0;
+ r[i++] = x;
+ if(x == y) {
+	exibe(r, i);
+ }else {
+	for(int j=0; j<n; j++) {
+	 if(v[j].origem == x	&& !pertence(v[j].destino, r, i))
+		rotas(v[j].destino, y, v, n);
+	}
+ }
+ i--;
+}
+
+int main(void){
+ Via v[10] = {{1,2},{1,3},{2,1},{2,4},{3,2},{3,4},{3,5},{4,3},{4,5},{5,4}};
+ rotas(1, 5, v, 10);
+ return 0;
+}
